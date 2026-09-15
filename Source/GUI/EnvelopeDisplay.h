f7c8a61335ae14519c0f6f687a00ffe0b00acd67
sergueyqqrr-lang <sergueyqqrr@gmail.com>
2026-09-15 00:11:06 -0400

    Refactor EnvelopeDisplay drawing methods
    Refactor EnvelopeDisplay class to improve drawing methods and add new functionalities.

--- Source/GUI/EnvelopeDisplay.h
+++ Source/GUI/EnvelopeDisplay.h
@@ -2,13 +2,6 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 #include "../PluginProcessor.h"
 
-/**
-    Dibuja las envolventes de Kick y Bass superpuestas en el tiempo.
-    Esta es la validación visual de la Etapa 1: si esto se ve bien
-    (el kick reacciona a los golpes, el bass reacciona al sidechain),
-    la base de análisis funciona y se puede pasar a la Etapa 2
-    (detectar solapamiento entre ambas curvas).
-*/
 class EnvelopeDisplay : public juce::Component, private juce::Timer
 {
 public:
@@ -20,13 +13,16 @@ public:
 
 private:
     void timerCallback() override;
-    void drawGrid (juce::Graphics&);
-    void drawEnvelopeTrace (juce::Graphics&, const std::array<float, KickBassSpaceAudioProcessor::historySize>& data,
-                             int writePos, juce::Colour colour, float alpha);
-    void drawSidechainWarning (juce::Graphics&);
+    void drawGrid (juce::Graphics&, juce::Rectangle<float> area);
+    void drawEnvelopeTrace (juce::Graphics&, juce::Rectangle<float> area,
+                             const std::array<float, KickBassSpaceAudioProcessor::historySize>& data,
+                             int writePos, juce::Colour colour, float alpha, bool filled);
+    void drawSidechainWarning (juce::Graphics&, juce::Rectangle<float> area);
+    void drawBandBreakdown (juce::Graphics&, juce::Rectangle<float> area);
+    void drawLegend (juce::Graphics&, juce::Rectangle<float> area);
 
     KickBassSpaceAudioProcessor& processor;
     KickBassSpaceAudioProcessor::EnvelopeSnapshot snapshot;
 
-    juce::Rectangle<float> plotArea;
+    juce::Rectangle<float> mainPlotArea, bandBreakdownArea;
 };
